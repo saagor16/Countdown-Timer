04c6975612f5c0ef2e3c9bbad6d89d560f3bfb4f
saagor16 <saagor.16@gmail.com>
2025-01-17 23:17:34 +0600

    ui create

--- src/Component/CountdownTimer/CountdownTimer.js
+++ src/Component/CountdownTimer/CountdownTimer.js
@@ -5,6 +5,15 @@ function CountdownTimer() {
   return (
     <div className='countdown-timer'>
       <h1>Countdown Timer</h1>
+      <div className='timer-display'>
+        <input type='number' placeholder='Enter time in minutes'></input>
+        <div>00:00</div>
+        <div className='timer-controls'>
+            <button>Start</button>
+            <button>Pause</button>
+            <button>reset</button>
+        </div>
+      </div>
     </div>
   )
 }
