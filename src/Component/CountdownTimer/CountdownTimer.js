14cfa4004e2fddb7d4b16a7541a80c702db4eec8
saagor16 <saagor.16@gmail.com>
2025-01-18 11:08:59 +0600

    time and settime create

--- src/Component/CountdownTimer/CountdownTimer.js
+++ src/Component/CountdownTimer/CountdownTimer.js
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './CountdownTimer.css'
 
 function CountdownTimer() {
+  const[time,setTime]=useState(0);
+
+  const handleInput=(event)=>{
+    setTime(parseInt(event.target.value*60))
+  }
+
+  const formatTime=()=>{
+    const min =String(Math.floor(time/60).padStart(2,0))
+  }
   return (
     <div className='countdown-timer'>
       <h1>Countdown Timer</h1>
       <div className='timer-display'>
-        <input type='number' placeholder='Enter time in minutes'></input>
-        <div>00:00</div>
+        <input type='number' placeholder='Enter time in minutes' onChange={handleInput}></input>
+        <div>{formatTime()}</div>
         <div className='timer-controls'>
             <button>Start</button>
             <button>Pause</button>
